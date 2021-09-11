aa4ffaf5d4552eb51f77763b7c0113922b8cb0c2
Benjamín Ayala <bayala0@ucol.mx>
2021-09-11 02:29:24 -0500

    feat(alert): alerts widget with action buttons

--- lib/src/pages/alert.dart
+++ lib/src/pages/alert.dart
@@ -11,6 +11,16 @@ class PageAlert extends StatelessWidget {
       appBar: AppBar(
         title: Text("Página de Alerta"),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Presióname :)"),
+          onPressed: () => _mostrarAlerta(context),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.amber,
+            //shape: StadiumBorder(),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
@@ -18,6 +28,55 @@ class PageAlert extends StatelessWidget {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Título de la alerta'),
+              CloseButton(
+                color: Color(0xFFD5D3D3),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("Contenido de la alerta"),
+              FlutterLogo(size: 100.0),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                primary: Colors.white,
+              ),
+              child: Text("Cancelar"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.amber,
+                primary: Colors.white,
+              ),
+              child: Text("Ok"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
     );
   }
 }
